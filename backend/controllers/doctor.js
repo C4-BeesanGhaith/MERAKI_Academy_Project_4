cb8b48fb27d0b989c04017951de387a65cce7b6a
BeesanGhaith <beesan.ghaith@gmail.com>
2022-01-10 23:00:48 +0200

    create Controller-Doctor

---
+++ backend/controllers/doctor.js
@@ -0,0 +1,26 @@
+const doctorsModel = require("../database/models/doctorsSchema");
+
+// This function creates a new doctor
+const createNewDoctor = (req, res) => {
+    const {firstName, lastName, age, jobTitle, phone, email, password} = req.body;
+
+    const doctor = new doctorsModel({
+        firstName,
+        lastName,
+        age,
+        jobTitle,
+        phone,
+        email,
+        password,
+    });
+    
+    doctor.save().then((result) => {
+        res.status(201).json({success: true, message: "Success Doctor Added", doctor: result});
+    }).catch((err) => {
+        
+    });
+};
+
+module.exports={
+    createNewDoctor,
+}
