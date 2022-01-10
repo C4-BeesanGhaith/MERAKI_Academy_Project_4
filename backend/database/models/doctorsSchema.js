2481743c6b2cc1c65ffab3c95a3b85fe28749ca0
BeesanGhaith <beesan.ghaith@gmail.com>
2022-01-10 22:59:38 +0200

    create DoctorSchema

---
+++ backend/database/models/doctorsSchema.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs")
+
+const doctorsSchema = new mongoose.Schema({
+  firstName: { type: String },
+  lastName: { type: String },
+  age: { type: Number },
+  jobTitle: { type: String },
+  phone: {type: String},
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+doctorsSchema.pre("save", async function () {
+    this.email = this.email.toLowerCase();
+    this.password = await bcrypt.hash(this.password, 10);
+  });
+
+  module.exports = mongoose.model("Doctor", doctorsSchema);
