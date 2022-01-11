6d1d55c3f5108affb8cc370924dfecffdb184f99
BeesanGhaith <beesan.ghaith@gmail.com>
2022-01-11 21:31:11 +0200

    createNewDiagnosis

---
+++ backend/controllers/diagnosis.js
@@ -0,0 +1,10 @@
+const patientsModel = require("../database/models/patientsSchema");
+const diagnosisModel = require("../database/models/diagnosisSchema");
+
+const createNewDiagnosis = (req, res) => {
+    // const 
+}
+
+module.exports = {
+    createNewDiagnosis,
+}
