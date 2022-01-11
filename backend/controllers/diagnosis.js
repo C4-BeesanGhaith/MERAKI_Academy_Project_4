5106357f809062492df07341b83c3b62b35d5203
BeesanGhaith <beesan.ghaith@gmail.com>
2022-01-12 00:13:28 +0200

    createNewDiagnosis

--- backend/controllers/diagnosis.js
+++ backend/controllers/diagnosis.js
@@ -2,7 +2,18 @@ const patientsModel = require("../database/models/patientsSchema");
 const diagnosisModel = require("../database/models/diagnosisSchema");
 
 const createNewDiagnosis = (req, res) => {
-    // const 
+    const patientId = req.params.id;
+    const {dateOfVisit, diagnosis, management} = req.body;
+
+    const diagnosis = new diagnosisModel({
+        dateOfVisit,
+        diagnosis,
+        management,
+    });
+
+    diagnosis.save().then((result) => {
+        
+    })
 }
 
 module.exports = {
