39967b98b601aa89c2812a7093692c1afa1ffdda
BeesanGhaith <beesan.ghaith@gmail.com>
2022-01-11 22:33:43 +0200

    deletePatientById

--- backend/controllers/patient.js
+++ backend/controllers/patient.js
@@ -55,9 +55,29 @@ const getAllPatients = (req, res) => {
     });
 };
 
-
+const deletePatientById = (req, res) => {
+    const id = req.params.id;
+    patientsModel.findByIdAndDelete(id).then((result) => {
+        if (!result) {
+            return res.status(404).json({
+                success: false,
+                message: `The Patient: ${id} is not found`,
+            });
+        }
+        res.status(200).json({
+            success: true,
+            message: `Succeeded to delete patient with id: ${id}`,
+        })
+    }).catch((err) => {
+        res.status(500).json({
+            success: false,
+            message: `Server Error`,
+        });
+    })
+}
 
 module.exports = {
   createNewPatient,
   getAllPatients,
+  deletePatientById,
 };
