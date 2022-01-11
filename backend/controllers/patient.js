36696ab0129f17a641c8ca670168a29881e026ed
BeesanGhaith <beesan.ghaith@gmail.com>
2022-01-11 21:32:21 +0200

    createNewPatient

--- backend/controllers/patient.js
+++ backend/controllers/patient.js
@@ -1,9 +1,43 @@
 const patientsModel = require("../database/models/patientsSchema");
 
-const createNewPatient = (req,res) => {
+const createNewPatient = (req, res) => {
+  const { firstName, lastName, age, country, phone } = req.body;
+  const patient = new patientsModel({
+    firstName,
+    lastName,
+    age,
+    country,
+    phone,
+    doctor: req.token.doctorId,
+  });
 
+  patient
+    .save()
+    .then((result) => {
+      res.status(201).json({
+        success: true,
+        message: "Patient created",
+        patient: result,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message: "Server Error",
+      });
+    });
 };
 
-module.exports = {
-    createNewPatient
+const getAllPatients = (req, res) => {
+    const doctorId = req.token.doctorId;
+    patientsModel.find({}).then((result) => {
+        if (result.length) {
+            res.status(200).json({})
+        }
+    })
 }
+
+module.exports = {
+  createNewPatient,
+  getAllPatients,
+};
