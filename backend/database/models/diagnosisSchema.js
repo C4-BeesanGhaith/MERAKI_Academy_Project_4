8f0dc7d494ee964bb23e99964bcb6fd32e04f81a
BeesanGhaith <beesan.ghaith@gmail.com>
2022-01-12 17:54:29 +0200

    update diagnosisSchema

--- backend/database/models/diagnosisSchema.js
+++ backend/database/models/diagnosisSchema.js
@@ -2,8 +2,10 @@ const mongoose = require("mongoose");
 
 const diagnosisSchema = new mongoose.Schema({
   dateOfVisit: { type: Date, default: Date.now() },
-  diagnosis: { type: String },
-  management: { type: String },
+  diagnosis: { type: String, required: true },
+  management: { type: String, required: true },
+  totalAmount:{type: String},
+  doctorSign: {type: String},
   doctor: { type: mongoose.Schema.Types.ObjectId, ref: "Doctor" },
 });
 
