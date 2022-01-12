6ae4077287ebb4b2a8574aff869b267c41395b87
BeesanGhaith <beesan.ghaith@gmail.com>
2022-01-12 16:27:58 +0200

    update patient.js-routes

--- backend/routes/patient.js
+++ backend/routes/patient.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const {createNewPatient, getAllPatients, deletePatientById, updatePatientById} = require("../controllers/patient");
 
+
+const { createNewDiagnosis } = require("../controllers/diagnosis");
+
 // Middleware
 const authentication = require("../middleware/authentication");
 
@@ -12,5 +15,7 @@ patientsRouter.post("/", authentication, createNewPatient);
 patientsRouter.get("/", authentication, getAllPatients);
 patientsRouter.delete("/:id", deletePatientById);
 patientsRouter.put("/:id", updatePatientById);
+
+patientsRouter.post("/:id/diagnosis", authentication, createNewDiagnosis);
 
 module.exports = patientsRouter;
