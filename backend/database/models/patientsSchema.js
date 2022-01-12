dc9f1d3de4d508317ab6bb368b04ac3a282fff8e
BeesanGhaith <beesan.ghaith@gmail.com>
2022-01-12 16:27:00 +0200

    update patientsSchema.js

--- backend/database/models/patientsSchema.js
+++ backend/database/models/patientsSchema.js
@@ -7,6 +7,7 @@ const patientsSchema = new mongoose.Schema({
   country: { type: String },
   phone: { type: String },
   doctor: { type: mongoose.Schema.Types.ObjectId, ref: "Doctor" },
+  diagnosiss: [{ type: mongoose.Schema.Types.ObjectId, ref: "Diagnosis" }],
   });
 
 module.exports = mongoose.model("Patients", patientsSchema);
