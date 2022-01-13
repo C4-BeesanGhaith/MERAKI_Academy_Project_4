d7087b7e74de9108eeb73d5baa6e6a7cbdf8fc40
BeesanGhaith <beesan.ghaith@gmail.com>
2022-01-14 00:09:28 +0200

    add context in index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import {BrowserRouter} from "react-router-dom"
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import AuthContext from "./Context/DoctorContext";
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <AuthContext>
+      <App />
+    </AuthContext>
   </BrowserRouter>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
