5edbb8faa240b8e99458f5fd8a2315d93c0f6a0c
BeesanGhaith <beesan.ghaith@gmail.com>
2022-01-14 18:19:56 +0200

    update patientsSchema

--- backend/database/models/patientsSchema.js
+++ backend/database/models/patientsSchema.js
@@ -1,12 +1,14 @@
 const mongoose = require("mongoose");
 
 const patientsSchema = new mongoose.Schema({
-  firstName: { type: String },
-  lastName: { type: String },
+  fullName: { type: String, required: true },
+  dateOfBirth: {type: String},
   age: { type: Number },
   gender: {type: String},
-  country: { type: String },
+  address: { type: String },
   phone: { type: String },
+  medicalHistory:{type: String},
+  allergy: {type: String},
   doctor: { type: mongoose.Schema.Types.ObjectId, ref: "Doctor" },
   diagnosiss: [{ type: mongoose.Schema.Types.ObjectId, ref: "Diagnosis" }],
   });
