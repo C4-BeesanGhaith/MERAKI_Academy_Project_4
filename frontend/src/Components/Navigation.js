5e91e372f22cc4698df549151938911e29c6f2c5
BeesanGhaith <beesan.ghaith@gmail.com>
2022-01-14 18:22:06 +0200

    add Link in Navigation page

--- frontend/src/Components/Navigation.js
+++ frontend/src/Components/Navigation.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../Context/DoctorContext";
 
 const Navigation = () => {
+  // data I need to pass from context file
+  const { isLoggedIn } = useContext(AuthContext);
   return (
     <>
       <div>
-        <Link to="/register">Register</Link>
-        <Link to="/login">Login</Link>
+        {isLoggedIn ? (
+          <>
+          <div>
+            <Link to="/about">About Us</Link>
+            <Link to="/patients">Patient List</Link>
+            <Link to="/new">New</Link>
+            <Link to="/open">Open</Link>
+            </div>
+          </>
+        ) : (
+          <>
+            <Link to="/register">Register</Link>
+            <Link to="/login">Login</Link>
+          </>
+        )}
       </div>
     </>
   );
