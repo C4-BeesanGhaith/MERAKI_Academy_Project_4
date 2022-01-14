339b8784c8fdd6b5b0eb92eb0ddca3f760ba5c5d
BeesanGhaith <beesan.ghaith@gmail.com>
2022-01-14 18:20:31 +0200

    add route in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import { Route, Routes } from "react-router-dom";
 import Register from "./Components/Register";
 import Navigation from "./Components/Navigation";
 import Login from "./Components/Login";
+import AboutUs from "./Components/AboutUs";
+import PatientList from "./Components/PatientList";
 
 
 function App() {
@@ -12,6 +14,8 @@ function App() {
       <Routes>
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login/>}/>
+        <Route path="/about" element={<AboutUs/>}/>
+        <Route path="/patients" element={<PatientList/>}/>
       </Routes>
     </div>
   );
