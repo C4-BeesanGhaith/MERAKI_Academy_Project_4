7ef7444817ef45a7230a8083a63815605b2b9874
BeesanGhaith <beesan.ghaith@gmail.com>
2022-01-18 17:50:59 +0200

    update DoctorSchema

--- backend/database/models/doctorsSchema.js
+++ backend/database/models/doctorsSchema.js
@@ -1,19 +1,20 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcrypt");
 
 const doctorsSchema = new mongoose.Schema({
   firstName: { type: String },
   lastName: { type: String },
+  dateOfBirth: { type: String },
   age: { type: Number },
   postTitle: { type: String },
-  phone: {type: String},
+  phone: { type: String },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });
 
 doctorsSchema.pre("save", async function () {
-    this.email = this.email.toLowerCase();
-    this.password = await bcrypt.hash(this.password, 10);
-  });
+  this.email = this.email.toLowerCase();
+  this.password = await bcrypt.hash(this.password, 10);
+});
 
-  module.exports = mongoose.model("Doctor", doctorsSchema);
+module.exports = mongoose.model("Doctor", doctorsSchema);
