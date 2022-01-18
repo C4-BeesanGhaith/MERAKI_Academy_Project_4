bb14d328c3e6f75d202825e84c27b4b71a4f4ec0
BeesanGhaith <beesan.ghaith@gmail.com>
2022-01-18 16:14:58 +0200

    add className to divs & invoke css page

--- frontend/src/Components/NewPatient.js
+++ frontend/src/Components/NewPatient.js
@@ -1,3 +1,4 @@
+import "./NewPatient.css";
 import axios from "axios";
 import React, { useContext, useState } from "react";
 import { AuthContext } from "../Context/DoctorContext";
@@ -13,141 +14,199 @@ const NewPatient = () => {
   const [medicalHistory, setMedicalHistory] = useState([]);
   const [allergy, setAllergy] = useState("");
   const [message, setMessage] = useState("");
+  const [status, setStatus] = useState(false);
 
   return (
     <>
-      <div>
-        <h1>Add New Patient</h1>
+      <div className="mainAddDiv">
+        <div className="addNewPatientDiv">
+          <i class="fas fa-users-medical" id="picAdd"></i>
+          <p className="paraAdd">Add New Patient</p>
+        </div>
+        <div className="informationDiv">
+          <div className="patientInfoDiv">
+            <p className="paraInfo">- Patient Information</p>
+            <div className="inputDiv">
+              <label>Full Name:</label>
+              <input
+                className="inputAdd"
+                type="text"
+                placeholder="Full Name ..."
+                onChange={(e) => {
+                  return setFullName(e.target.value);
+                }}
+              />
+              <label>Date Birth:</label>
+              <input
+                className="inputAdd"
+                type="date"
+                placeholder="Date Of Birth ..."
+                onChange={(e) => {
+                  return setDateOfBirth(e.target.value);
+                }}
+              />
+              <label>Age:</label>
+              <input
+                className="inputAdd"
+                type="number"
+                placeholder="Age ..."
+                onChange={(e) => {
+                  return setAge(e.target.value);
+                }}
+              />
+              <label>Gender:</label>
+              <div>
+                <input
+                  type="radio"
+                  name="gender"
+                  value="Femail"
+                  onChange={(e) => {
+                    return setGender(e.target.value);
+                  }}
+                />
+                <label>Femail</label>
+                <input
+                  className="radioAdd"
+                  type="radio"
+                  name="gender"
+                  value="Mail"
+                  onChange={(e) => {
+                    return setGender(e.target.value);
+                  }}
+                />
+                <label>Mail</label>
+              </div>
+              <label>Address:</label>
+              <input
+                className="inputAdd"
+                type="text"
+                placeholder="Address ..."
+                onChange={(e) => {
+                  return setAddress(e.target.value);
+                }}
+              />
+              <label>Phone Number:</label>
+              <input
+                className="inputAdd"
+                type="text"
+                placeholder="Phone Number ..."
+                onChange={(e) => {
+                  return setPhone(e.target.value);
+                }}
+              />
+            </div>
+          </div>
+          <br />
+          <div className="patientInfoDiv">
+            <p className="paraInfo">- Medical Information</p>
+            <div className="inputDiv_2">
+              <label>Medical History:</label>
+              <div className="checkboxDiv">
+                <input
+                  type="checkbox"
+                  name="medicalHistory"
+                  value="Diabetes"
+                  onChange={(e) => {
+                    return setMedicalHistory(e.target.value);
+                  }}
+                />
+                <label>Diabetes</label>
+                <input
+                  className="box2"
+                  type="checkbox"
+                  name="medicalHistory"
+                  value="Hypertension"
+                  onChange={(e) => {
+                    return setMedicalHistory(e.target.value);
+                  }}
+                />
+                <label>Hypertension</label>
+                <input
+                  className="box2"
+                  type="checkbox"
+                  name="medicalHistory"
+                  value="Asthma"
+                  onChange={(e) => {
+                    return setMedicalHistory(e.target.value);
+                  }}
+                />
+                <label>Asthma</label>
+                <input
+                  className="box2"
+                  type="checkbox"
+                  name="medicalHistory"
+                  value="Drug Allergy"
+                  onChange={(e) => {
+                    return setMedicalHistory(e.target.value);
+                  }}
+                />
+                <label>Drug Allergy</label>
+                <br />
+              </div>
+              <label>Allergy:</label>
+              <div>
+                <input
+                  type="radio"
+                  name="allergy"
+                  value="Yes"
+                  onChange={(e) => {
+                    return setAllergy(e.target.value);
+                  }}
+                />
+                <label>Yes</label>
+                <input
+                  className="radioAdd"
+                  type="radio"
+                  name="allergy"
+                  value="No"
+                  onChange={(e) => {
+                    return setAllergy(e.target.value);
+                  }}
+                />
+                <label>No</label>
+              </div>
+              <br />
+            </div>
+          </div>
+            <button
+              className="btnAdd"
+              onClick={() => {
+                axios
+                  .post(
+                    "http://localhost:5000/patients/",
+                    {
+                      fullName,
+                      dateOfBirth,
+                      age,
+                      gender,
+                      address,
+                      phone,
+                      medicalHistory,
+                      allergy,
+                    },
+                    {
+                      headers: {
+                        authorization: `Bearer ${token}`,
+                      },
+                    }
+                  )
+                  .then((response) => {
+                    setMessage(response.data.message);
+                    setStatus(true);
+                  })
+                  .catch((err) => {
+                    setMessage(err.response.data.message);
+                    setStatus(false);
+                  });
+              }}
+            >
+              Submit
+            </button>
+            {status
+              ? message && <div className="SuccessMessage">{message}</div>
+              : message && <div className="ErrorMessage">{message}</div>}
+        </div>
       </div>
-      <div>
-        <p>Patient Information</p>
-        <label>Full Name</label>
-        <input
-          type={"text"}
-          placeholder="Full Name ..."
-          onChange={(e) => {
-            return setFullName(e.target.value);
-          }}
-        />
-        <br />
-        <label>Date Birth</label>
-        <input
-          type={"date"}
-          placeholder="Date Of Birth ..."
-          onChange={(e) => {
-            return setDateOfBirth(e.target.value);
-          }}
-        />
-        <br />
-        <label>Age</label>
-        <input
-          type={"number"}
-          placeholder="Age ..."
-          onChange={(e) => {
-            return setAge(e.target.value);
-          }}
-        />
-        <br />
-        <label>Gender</label>
-        <input
-          type={"radio"}
-          name="gender"
-          value={"Femail"}
-          onChange={(e) => {
-            return setGender(e.target.value);
-          }}
-        />
-        <label>Femail</label>
-        <input
-          type={"radio"}
-          name="gender"
-          value={"Mail"}
-          onChange={(e) => {
-            return setGender(e.target.value);
-          }}
-        />
-        <label>Mail</label>
-        <br />
-        <label>Address</label>
-        <input
-          type={"text"}
-          placeholder="Address ..."
-          onChange={(e) => {
-            return setAddress(e.target.value);
-          }}
-        />
-        <br />
-        <label>Phone Number</label>
-        <input
-          type={"text"}
-          placeholder="Phone Number ..."
-          onChange={(e) => {
-            return setPhone(e.target.value);
-          }}
-        />
-        <br />
-        <p>Medical Information</p>
-        <label>Medical History</label>
-        <input
-          type={"checkbox"}
-          name="medicalHistory"
-          value={"Diabetes"}
-          onChange={(e) => {
-            return setMedicalHistory(e.target.value);
-          }}
-        />
-        <label>Diabetes</label>
-        <input
-          type={"checkbox"}
-          name="medicalHistory"
-          value={"Hypertension"}
-          onChange={(e) => {
-            return setMedicalHistory(e.target.value);
-          }}
-        />
-        <label>Hypertension</label>
-        <br />
-        <label>Allergy</label>
-        <select
-          onChange={(e) => {
-            console.log(allergy);
-            return setAllergy(e.target.value);
-          }}
-        >
-          <option value={"Yes"}>Yes</option>
-          <option value={"No"}>No</option>
-        </select>
-        <br />
-        <button
-          onClick={() => {
-            axios.post(
-              "http://localhost:5000/patients/",
-              {
-                fullName,
-                dateOfBirth,
-                age,
-                gender,
-                address,
-                phone,
-                medicalHistory,
-                allergy,
-              },
-              {
-                headers: {
-                  authorization: `Bearer ${token}`,
-                },
-              }
-            ).then((response) => {
-                setMessage(response.data.message);
-            }).catch((err) => {
-                setMessage(err.response.data.message);
-            });
-          }}
-        >
-          Submit
-        </button>
-      </div>
-      {message && <p>{message}</p>}
     </>
   );
 };
