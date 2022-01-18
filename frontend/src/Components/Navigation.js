7cd6221fa3c22daf57dd17d8aec54b76a6c2ee19
BeesanGhaith <beesan.ghaith@gmail.com>
2022-01-18 17:54:41 +0200

    update Navigation.js

--- frontend/src/Components/Navigation.js
+++ frontend/src/Components/Navigation.js
@@ -10,7 +10,7 @@ const Navigation = () => {
     <>
       <div className="header">
       <i class="fas fa-hospital-user" id="pic"></i>
-      {/* <i class="fas fa-users-medical" id="pic"></i> */}
+      {/* <i class="fas fa-user-md" id="pic"></i> */}
         <p className="paraTitle">FAMILY</p><p className="paraTitle2">CLINIC</p>
         {isLoggedIn ? (
           <>
@@ -18,16 +18,17 @@ const Navigation = () => {
               <Link to="/about" className="link">
                 About Us
               </Link>
-              <Link to="/patients">Patient List</Link>
-              <Link to="/new">New</Link>
-              <Link to="/diagnosis">Diagnosis Page</Link>
+              <div><i class="fad fa-clipboard-list-check" id="listPic"></i>
+              <Link to="/patients" className="link">Patient List</Link></div>
+              <Link to="/new" className="link">New</Link>
+              <Link to="/diagnosis" className="link">Diagnosis Page</Link>
             </div>
           </>
         ) : (
           <>
             <div className="links">
-              <Link to="/register">Register</Link>
-              <Link to="/login">Login</Link>
+              <Link to="/register" className="link">Register</Link>
+              <Link to="/login" className="link">Login</Link>
             </div>
           </>
         )}
