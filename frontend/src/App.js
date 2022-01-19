3eec1d535cd69dd9182b45ea97516197663c4c37
BeesanGhaith <beesan.ghaith@gmail.com>
2022-01-20 01:38:23 +0200

    import Appointment in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,10 @@ import { Route, Routes } from "react-router-dom";
 import Register from "./Components/Register";
 import Navigation from "./Components/Navigation";
 import Login from "./Components/Login";
-import AboutUs from "./Components/AboutUs";
 import PatientList from "./Components/PatientList";
 import NewPatient from "./Components/NewPatient";
 import Diagnosis from "./Components/Diagnosis";
+import Appointment from "./Components/Appointment";
 
 
 function App() {
@@ -16,7 +16,7 @@ function App() {
       <Routes>
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login/>}/>
-        <Route path="/about" element={<AboutUs/>}/>
+        <Route path="/appointment" element={<Appointment/>}/>
         <Route path="/patients" element={<PatientList/>}/>
         <Route path="/new" element={<NewPatient/>}/>
         <Route path="/diagnosis" element={<Diagnosis/>}/>
